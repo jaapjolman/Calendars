7a4724765651b76d034d0cbce0619a24f589f80b
Jacob Albert Jolman <jaap@jolman.eu>
2013-01-21 20:35:42 +0100

    Dutch Language Added

---
+++ language/dutch/calendars_lang.php
@@ -0,0 +1,152 @@
+<?php
+
+
+// Fields
+$lang['calendars:str_id']									= 'Tekenreeks ID';
+$lang['calendars:default']									= 'Standaard';
+$lang['calendars:title']									= 'Titel';
+$lang['calendars:slug']										= 'Slug';
+$lang['calendars:notes']									= 'Notities';
+$lang['calendars:description']								= 'Omschrijving';
+$lang['calendars:bg_color']									= 'Achtergrond Kleur';
+$lang['calendars:border_color']								= 'Rand Kleur';
+$lang['calendars:text_color']								= 'Tekst Kleur';
+$lang['calendars:privacy']									= 'Privacy';
+$lang['calendars:shared']									= 'Gedeeld';
+$lang['calendars:gc_id']									= 'Google Calender ID';
+$lang['calendars:feed_url']									= 'Feed URL';
+$lang['calendars:calendar']									= 'Calender';
+$lang['calendars:location']									= 'Locatie';
+$lang['calendars:all_day']									= 'De heledag';
+$lang['calendars:starting']									= 'Begind';
+$lang['calendars:ending']									= 'eindigd';
+$lang['calendars:busy']										= 'Bezig';
+$lang['calendars:repeating']								= 'Herhalend';
+$lang['calendars:repeat_span']								= 'Herhaling';
+$lang['calendars:repeats_every']							= 'Herhaling Vermelding';
+$lang['calendars:repeating_ends']							= 'Herhaling eindigd';
+$lang['calendars:event_image']								= 'Afbeelding';
+$lang['calendars:calendar_image']							= 'Afbeelding';
+
+
+
+// Instructions
+$lang['calendars:instructions:str_id']						= '';
+$lang['calendars:instructions:title']						= '';
+$lang['calendars:instructions:slug']						= '';
+$lang['calendars:instructions:notes']						= 'Alleen voor intern gebruik.';
+$lang['calendars:instructions:description']					= 'Dit wordt op uw website getoont onder Publieke gebeurtenissen.';
+$lang['calendars:instructions:bg_color']					= 'Kies een achtergrond kleur voor deze calender.';
+$lang['calendars:instructions:border_color']				= 'Kies een rand kleur voor deze calender.';
+$lang['calendars:instructions:text_color']					= 'Kies een tekst kleur voor deze calender.';
+$lang['calendars:instructions:privacy']						= 'Alleen Publieke calenders kunnen op uw website worden bekeken. Privé calenders zullen zich aan de gedeelde limiten houden.';
+$lang['calendars:instructions:shared']						= 'Wilt u andere gebruikers toegang geven tot deze calender?';
+$lang['calendars:instructions:gc_id']						= 'Volg deze instructies.';
+$lang['calendars:instructions:feed_url']					= 'Voer een URL in voor een aangepast gebeurtenis feeds.';
+$lang['calendars:instructions:calendar']					= '';
+$lang['calendars:instructions:location']					= '';
+$lang['calendars:instructions:all_day']						= '';
+$lang['calendars:instructions:starting']					= '';
+$lang['calendars:instructions:ending']						= '';
+$lang['calendars:instructions:busy']						= '';
+$lang['calendars:instructions:repeating']					= '';
+$lang['calendars:instructions:repeat_span']					= '';
+$lang['calendars:instructions:repeats_every']				= '';
+$lang['calendars:instructions:repeating_ends']				= 'Laat dit leeg om hem oneindig vaak te herhalen.';
+$lang['calendars:instructions:event_image']					= '';
+$lang['calendars:instructions:calendar_image']				= '';
+
+
+
+// Choice Data
+$lang['calendars:choice:privacy']							= "0 : Pirvé\n1 : Publiek";
+$lang['calendars:choice:shared']							= "1 : Andere mensen toegang geven tot deze calender.";
+$lang['calendars:choice:all_day']							= "1 : Ja, deze gebeurtenis duurt de heledag.";
+$lang['calendars:choice:repeating']							= "1 : Ja, dit is een herhalende gebeurtenis.";
+$lang['calendars:choice:repeat_span']						= "D : Dagelijks\nW : Wekelijks\nM : Maandelijks\nY : Jaarlijks";		// Do not change the D, W, M or Y keys...
+
+
+
+// Titles
+$lang['calendars:title:calendars']							= 'Calenders';
+$lang['calendars:title:create_calendar']					= 'Calender Aanmaken';
+$lang['calendars:title:edit_calendar']						= 'Calender Bewerken';
+$lang['calendars:title:events']								= 'Gebeurtenissen';
+$lang['calendars:title:create_event']						= 'Gebeurtenis Aanmaken';
+$lang['calendars:title:edit_event']							= 'Gebeurtenis Bewerken';
+$lang['calendars:title:view_event']							= 'Gebeurtenis Bekijken';
+$lang['calendars:title:jump_to']							= 'Spring naar...';
+$lang['calendars:title:calendar_for']						= 'Calender voor';
+$lang['calendars:title:event_information']					= 'Gebeurtenis Informatie';
+$lang['calendars:title:tasks']								= 'Taak';
+
+
+
+// Tabs
+$lang['calendars:tab:calendar']								= 'Calender';
+$lang['calendars:tab:sharing']								= 'Gedeeld';
+$lang['calendars:tab:other']								= 'Anders';
+$lang['calendars:tab:event']								= 'Gebeurtenis';
+$lang['calendars:tab:repeating']							= 'Herhaaling';
+$lang['calendars:tab:content']								= 'Inhoud';
+$lang['calendars:tab:comments']								= 'Commentaar';
+$lang['calendars:tab:tasks']								= 'Taak';
+
+
+
+// Sections
+$lang['calendars:section:calendars']						= 'Calenders';
+$lang['calendars:section:events']							= 'Gebeurtenissen';
+
+
+
+// Error notifications
+$lang['calendars:error:no_calendars']	 					= 'Maak alstublieft eerst een calender.';
+$lang['calendars:error:install_tasks']	 					= 'Installeer alstublief eerst module Taken.';
+
+
+
+// Buttons
+$lang['calendars:button:new_calendar']						= 'Nieuwe Calender';
+$lang['calendars:button:new_event']							= 'Nieuwe Gebeurtenis';
+$lang['calendars:button:edit_event']						= 'Bewerk Gebeurtenis';
+$lang['calendars:button:delete']							= 'Verwijder';
+$lang['calendars:button:show_all']							= 'Toon Alle';
+$lang['calendars:button:hide_all']							= 'Verberg Alle';
+$lang['calendars:button:today']								= 'Vandaag';
+$lang['calendars:button:calendars']							= 'Calenders';
+$lang['calendars:button:mini_calendar']						= 'Mini Calender';
+$lang['calendars:button:month']								= 'Maand';
+$lang['calendars:button:week']								= 'Week';
+$lang['calendars:button:day']								= 'Dag';
+$lang['calendars:button:daily_agenda']						= 'Dag Agenda';
+$lang['calendars:button:weekly_agenda']						= 'Week Agenda';
+
+
+
+// Links
+$lang['calendars:link:edit_this_event']						= 'Bewerk deze gebeurtenis';
+
+
+
+// Folders
+$lang['calendars:folder:calendars_module']					= 'Calender Module';
+$lang['calendars:folder:events']							= 'Gebeurtenissen';
+$lang['calendars:folder:calendars']							= 'Calenders';
+
+
+
+
+/*
+ *	Miscellaneous Stuff
+ ----------------------------------------------*/
+
+
+// (date) from (time)
+$lang['calendars:misc:from_space']							= ' van ';
+
+// (time) to (time)
+$lang['calendars:misc:to_space']							= ' tot ';
+
+// All Day
+$lang['calendars:misc:all_day']								= 'Hele dag';
